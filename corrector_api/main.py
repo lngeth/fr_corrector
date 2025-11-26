9d8e74b12977bc2fdaae2d29d3bafce7566e5f41
Laurent Ngeth <l.ngeth@yahoo.fr>
2025-11-26 12:12:00 +0100

    add health check & implement with docker compose

--- corrector_api/main.py
+++ corrector_api/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from models import TextRequest
-from routers import grammar
+from routers import grammar, api_health
 
-app = FastAPI()
+title = "French Corrector API"
+description = "API that use LLM from HuggingFace to correct spelling mistakes in French texts."
 
+app = FastAPI(
+  title=title,
+  description=description
+)
+
+app.include_router(api_health.router)
 app.include_router(grammar.router)
--- corrector_api/routers/grammar.py
+++ corrector_api/routers/grammar.py
@@ -5,6 +5,6 @@ from services.text_correction import correct_text
 router = APIRouter()
 
 @router.post("/correct_grammar_fr")
-def correct_grammar_fr_endpoint(request: TextRequest):
+def correct_grammar_from_text_fr(request: TextRequest):
   corrected_text = correct_text(request.text)
   return {"corrected_text": corrected_text.text}
