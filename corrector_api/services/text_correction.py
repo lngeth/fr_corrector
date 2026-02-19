99f97363b34149419c39ad15ba477def1ef631fb
Laurent Ngeth <l.ngeth@yahoo.fr>
2026-02-19 13:44:53 +0100

    change path model

--- corrector_api/services/text_correction.py
+++ corrector_api/services/text_correction.py
@@ -1,11 +1,7 @@
 from happytransformer import HappyTextToText, TTSettings
 
 # Charger depuis le dossier local
-happy_tt = HappyTextToText(
-    model_type="T5",
-    model_name="t5-base",
-    load_path="./corrector_api/model"
-)
+happy_tt = HappyTextToText("T5", "corrector_api/model")
 args = TTSettings(num_beams=5, min_length=1)
 
 def correct_text(text: str) -> str:
